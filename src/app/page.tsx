98099d9e7860e7062d536e3567f82a101ecd39a8
aac <s65052@bht-berlin.de>
2024-09-03 14:35:25 +0200

    footer funktionalität wieder hergestellt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { Header } from "@/sections/Header";
 import { Hero } from "@/sections/Hero";
-import { Footer } from "@/sections/Footer";
 import { CallToAction } from "@/sections/CallToAction";
 import { Comments } from "@/sections/Comments";
 import { Biography } from "@/sections/Biograpy";
@@ -10,6 +9,7 @@ import { Skills } from "@/sections/Skills";
 import { Contact } from "@/sections/sectionsAbout/Contact";
 import { Projects } from "@/sections/Projects";
 import { Tape } from "@/sections/Tape";
+import { Footer } from "@/sections/Footer";
 
 export default function Home() {
   return (
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -5,18 +5,20 @@ import Github from "@/assets/socials/social-github.svg";
 
 export const Footer = () => {
   return (
-    <footer className="relative -z-10 overflow-x-clip">
-      <div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-gradient-to-r bg-orange-500/20 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10]"></div>
-      <div className="container">
-        <div className="border-t border-white/15 py-6 text-sm flex flex-col lg:flex-row md:flex-row md:justify-between item-center gap-8">
+    <footer className="relative py-6 overflow-hidden">
+      <div className="absolute inset-0 pointer-events-none">
+        <div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-gradient-to-r from-orange-500/20 to-transparent [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)]"></div>
+      </div>
+      <div className="container mx-auto relative z-10">
+        <div className="border-t border-white/15 py-6 text-sm flex flex-col lg:flex-row md:flex-row md:justify-between items-center gap-8">
           <div className="flex flex-col md:flex-row items-center gap-8">
-            <Logo className="h-6 w-6" />
-            <div className="font-medium">AAC portfolio</div>
+            <Logo className="h-6 w-6 text-white" />
+            <div className="font-medium text-white">AAC portfolio</div>
           </div>
           <nav className="flex flex-col md:flex-row items-center gap-8">
             <a
-              href="#"
-              className="text-white/70 hover:text-white text-xs md:text-sm transition"
+              href="#home"
+              className="text-white/70 hover:text-white text-xs md:text-sm transition duration-300"
             >
               AA&AF Chizari
             </a>
@@ -26,22 +28,28 @@ export const Footer = () => {
               href="https://www.linkedin.com/in/amir-chizari-2047192a7/"
               target="_blank"
               rel="noopener noreferrer"
+              aria-label="LinkedIn Profil"
+              className="text-white/70 hover:text-white transition duration-300 transform hover:scale-150"
             >
-              <Linkedin className="text-white/70 hover:text-white transition" />
+              <Linkedin className="w-6 h-6" />
             </a>
             <a
               href="https://www.xing.com/profile/Amir_Chizari061776/web_profiles?expandNeffi=true"
               target="_blank"
               rel="noopener noreferrer"
+              aria-label="Xing Profil"
+              className="text-white/70 hover:text-white transition duration-300 transform hover:scale-150"
             >
-              <Xing className="text-white/70 hover:text-white transition" />
+              <Xing className="w-6 h- 6" />
             </a>
             <a
               href="https://github.com/AAChizari"
               target="_blank"
               rel="noopener noreferrer"
+              aria-label="GitHub Profil"
+              className="text-white/70 hover:text-white transition duration-300 transform hover:scale-150"
             >
-              <Github className="text-white/70 hover:text-white transition" />
+              <Github className="w-6 h-6" />
             </a>
           </div>
         </div>
