cb3ca3f47453d92d9fbdf7e26a0f264b96f74a53
aac <s65052@bht-berlin.de>
2024-09-03 11:49:34 +0200

    tape anpassung

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -12,7 +12,7 @@ export const Tape = () => {
       <div className="bg-gradient-to-r from-[#FFB16D] to-[#EC6F00] -rotate-3 -mx-1">
         <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
           <div className="flex flex-none gap-6 pr-6 py-3 animate-move-left [animation-duration:30s]">
-            {[...new Array(4)].map((_, index) => (
+            {[...new Array(8)].map((_, index) => (
               <Fragment key={index}>
                 {words.map((word) => (
                   <div key={word} className="inline-flex items-center gap-4">
